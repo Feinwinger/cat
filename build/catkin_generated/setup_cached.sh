cacc8e7196f1419f63a3cb6d1b9c563e48c6a25d
Feinwinger <hao-ji@outlook.com>
2018-07-23 10:14:52 +0200

    cat

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/hao/roscat/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/hao/roscat/devel/lib:$LD_LIBRARY_PATH"
+export PWD="/home/hao/roscat/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hao/roscat/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/hao/roscat/src:$ROS_PACKAGE_PATH"
